5585424c26afd2d3e2e5242b9c54565a225f3e35
root <root@DESKTOP-R16CEVD.localdomain>
2019-11-28 11:29:21 +0100

    v1

---
+++ flgaz-master/app2.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template, redirect, url_for
+import csv
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
+@app.route('/')
+def home():
+    return 'Bienvenue !'
+
+@app.route('/gaz', methods=['GET','POST'])
+def save_gazouille():
+	if request.method == 'POST':
+		print(request.form)
+		dump_to_csv(request.form)
+		return redirect(url_for('timeline'))
+		#return "OK"
+	if request.method == 'GET':
+		return render_template('formulaire.html')
+
+@app.route('/timeline', methods=['GET'])
+def timeline():
+	gaz = parse_from_csv()
+	return render_template("timeline.html", gaz = gaz)
+
+def parse_from_csv():
+	gaz = []
+	with open('./gazouilles.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			gaz.append({"user":row[0], "text":row[1]})
+	return gaz
+
+def dump_to_csv(d):
+	donnees = [d["user-name"],d["user-text"] ]
+	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+		writer = csv.writer(f)
+		writer.writerow(donnees)
+
+@app.route("/slow")
+@limiter.limit("1 per day")
+def slow():
+    return "24"
+
+@app.route("/fast")
+def fast():
+    return "42"
+
+@app.route("/ping")
+@limiter.exempt
+def ping():
+    return "PONG"
